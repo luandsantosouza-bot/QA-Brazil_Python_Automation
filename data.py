524d51f1a59947d530035756cdf9f09b3a600b3e
Luan dos Santos Souza <luandsantosouza@gmail.com>
2025-09-21 17:00:23 +0100

    Add files via upload

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# Arquivo com constantes
+URBAN_ROUTES_URL = "https://cnt-4a5f7706-336b-4fa2-bd9e-80e983155c18.containerhub.tripleten-services.com?lng=pt"
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO =	'1300 1st St'
+PHONE_NUMBER =	'+1 123 123 12 12'
+CARD_NUMBER =	'1234 5678 9100'
+CARD_CODE =	'1111'
+MESSAGE_FOR_DRIVER =	'Pare no bar de sucos'
